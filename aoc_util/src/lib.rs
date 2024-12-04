4c9e1be54de1b3723bc3099f5928e410c21c752f
Kai Roßwag <kai.rosswag@horus.biz>
2024-12-05 00:49:48 +0100

    proper bench.

--- aoc_util/src/lib.rs
+++ aoc_util/src/lib.rs
@@ -1,3 +1,42 @@
+use std::slice::Iter;
+use CardinalDirection::*;
+#[derive(Clone, Copy, Debug)]
+pub enum CardinalDirection {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
+
+impl CardinalDirection {
+    const ALL_VALUES: [CardinalDirection; 8] = [
+        North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest,
+    ];
+
+    pub fn direction_vec(&self) -> (i32, i32) {
+        use CardinalDirection::*;
+        match self {
+            North => (0, -1),
+            NorthEast => (1, -1),
+            East => (1, 0),
+            SouthEast => (1, 1),
+            South => (0, 1),
+            SouthWest => (-1, 1),
+            West => (-1, 0),
+            NorthWest => (-1, -1),
+        }
+    }
+
+    pub fn iter() -> Iter<'static, CardinalDirection> {
+        Self::ALL_VALUES.iter()
+    }
+
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -29,117 +29,16 @@ where
     (count_p1, count_p2)
 }
 
-#[derive(Clone, Copy, Debug)]
-pub struct SpiralArm {
-    direction: SpirallingOutOfControl,
-    hor: i32,
-    vert: i32,
-}
-#[derive(Clone, Copy, Debug)]
-enum SpirallingOutOfControl {
-    North,
-    NorthEast,
-    East,
-    SouthEast,
-    South,
-    SouthWest,
-    West,
-    NorthWest,
-}
-
-impl SpiralArm {
-    fn all_cardinalities() -> Vec<SpiralArm> {
-        use SpirallingOutOfControl::*;
-        Vec::from([
-            Self::n(North),
-            Self::n(NorthEast),
-            Self::n(East),
-            Self::n(SouthEast),
-            Self::n(South),
-            Self::n(SouthWest),
-            Self::n(West),
-            Self::n(NorthWest),
-        ])
-    }
-
-    fn n(direction: SpirallingOutOfControl) -> SpiralArm {
-        SpiralArm {
-            direction,
-            hor: 0,
-            vert: 0,
-        }
-    }
-    fn further_out(&mut self) {
-        use SpirallingOutOfControl::*;
-        match self.direction {
-            North => {
-                self.vert -= 1;
-            }
-            NorthEast => {
-                self.hor += 1;
-                self.vert -= 1;
-            }
-            East => {
-                self.hor += 1;
-            }
-            SouthEast => {
-                self.hor += 1;
-                self.vert += 1;
-            }
-            South => {
-                self.vert += 1;
-            }
-            SouthWest => {
-                self.hor -= 1;
-                self.vert += 1;
-            }
-            West => {
-                self.hor -= 1;
-            }
-            NorthWest => {
-                self.hor -= 1;
-                self.vert -= 1;
-            }
-        }
-    }
-
-    fn in_bounds_and_proper(
-        &self,
-        expected: u8,
-        hor: usize,
-        vert: usize,
-        grid: &Vec<Vec<u8>>,
-    ) -> bool {
-        if vert as i32 + self.vert < 0 {
-            return false;
-        }
-        let vert_pos = (vert as i32 + self.vert) as usize;
-        if let Some(line) = grid.get(vert_pos) {
-            if hor as i32 + self.hor < 0 {
-                return false;
-            }
-            let hor_pos = (hor as i32 + self.hor) as usize;
-            if let Some(val) = line.get(hor_pos) {
-                *val == expected
-            } else {
-                false
-            }
-        } else {
-            false
-        }
-    }
-}
-
 fn find_matches_p1(grid: &Vec<Vec<u8>>, hor: usize, vert: usize) -> usize {
     let mut direction_found = 0;
-    'outer: for mut direction in SpiralArm::all_cardinalities() {
-        for val in [b'M', b'A', b'S'] {
-            direction.further_out();
-            if !direction.in_bounds_and_proper(val, hor, vert, grid) {
-                continue 'outer;
+    for direction in CardinalDirection::iter() {
+        if let Some(b'M') = get_in_bounds(grid, hor, vert, *direction, 1) {
+            if let Some(b'A') = get_in_bounds(grid, hor, vert, *direction, 2) {
+                if let Some(b'S') = get_in_bounds(grid, hor, vert, *direction, 3) {
+                    direction_found += 1;
+                }
             }
         }
-        direction_found += 1;
     }
 
     direction_found

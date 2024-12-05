9a1af724a1c8d6b90d42853340345a22761e98d2
Kai Roßwag <kai.rosswag@horus.biz>
2024-12-05 22:47:35 +0100

    Solve day05.

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -25,11 +25,14 @@ where
     }
 
     let mut count_p1: usize = 0;
-    'outer: for update in &updates {
+    let mut count_p2 = 0;
+    for update in &updates {
         let mut forbidden = HashSet::new();
+        let mut valid = true;
         for page in update {
             if forbidden.contains(&page) {
-                continue 'outer;
+                valid = false;
+                break;
             } else {
                 if let Some(followers) = po_rules.get(page) {
                     for follower in followers {
@@ -38,11 +41,42 @@ where
                 }
             }
         }
-        count_p1 += update[update.len() / 2] as usize;
+        if valid {
+            count_p1 += update[update.len() / 2] as usize;
+        } else {
+            let mut forbidden_stack = vec![HashSet::new()];
+            let mut maybe_ordered = Vec::new();
+            for page in update {
+                flansch(&mut forbidden_stack, page, &mut maybe_ordered, &po_rules);
+            }
+            count_p2 += maybe_ordered[maybe_ordered.len() / 2] as usize
+        }
     }
-
-    let count_p2 = 5;
-
     (count_p1, count_p2)
+}
+
+fn flansch(forbidden_stack: &mut Vec<HashSet<u8>>, page: &u8, maybe_ordered: &mut Vec<u8>, po_rules: &HashMap<u8, Vec<u8>>) {
+    // println!("Curr page: {page}");
+    let mut helper_stack = Vec::new();
+    while forbidden_stack.last().unwrap().contains(page) {
+        forbidden_stack.pop();
+        let minus = maybe_ordered.pop().unwrap();
+        helper_stack.push(minus);
+        // println!("{:?} - {minus}", maybe_ordered);
+    }
+    maybe_ordered.push(*page);
+    let mut new_forbidden = forbidden_stack.last().unwrap().clone();
+    if let Some(followers) = po_rules.get(page) {
+        for follower in followers {
+            new_forbidden.insert(*follower);
+        }
+    }
+    forbidden_stack.push(new_forbidden);
+    // println!("Helper: {:?}", helper_stack);
+    for idx in 0..helper_stack.len() {
+        flansch(forbidden_stack, &helper_stack[idx], maybe_ordered, po_rules);
+        // println!("{:?} + {val}", maybe_ordered);
+    }
+    // println!("End: {:?}", maybe_ordered);
 }
 

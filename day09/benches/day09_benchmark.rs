0e14de07bdf683b4a68d2b85c96451e9ca7d159c
Kai Roßwag <kai.rosswag@horus.biz>
2024-12-09 23:14:26 +0100

    fix benches

--- day09/benches/day09_benchmark.rs
+++ day09/benches/day09_benchmark.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 pub fn criterion_benchmark(c: &mut Criterion) {
     let input = include_bytes!("../../input/day09");
-    c.bench_function("bench day08 real", |b| b.iter(|| day09::run(black_box(&input[..]))));
+    c.bench_function("bench day09 real", |b| b.iter(|| day09::run(black_box(&input[..]))));
 }
 
 criterion_group!(benches, criterion_benchmark);

7d7d7b88fa4cfd4527a0be5723b669f51bc847f9
Kai Roßwag <kai.rosswag@horus.biz>
2024-12-18 00:54:51 +0100

    Solve part 2 beautifully.

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -2,51 +2,67 @@ use itertools::Itertools;
 use std::io::BufRead;
 
 struct Computer {
-    pc: usize,
-    reg_a: u32,
-    reg_b: u32,
-    reg_c: u32,
+    state: ComputerState,
 
-    instructions: Vec<u32>,
+    instructions: Vec<u64>,
     output: Vec<u8>,
+}
+
+struct ComputerState {
+    pc: usize,
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
 }
 
 impl Computer {
     pub fn run_program(&mut self) {
-        while self.pc < self.instructions.len() {
-            let instruction = self.instructions[self.pc];
-            let operand = self.instructions[self.pc + 1];
+        while self.state.pc < self.instructions.len() {
+            let instruction = self.instructions[self.state.pc];
+            let operand = self.instructions[self.state.pc + 1];
 
-            match instruction {
-                0 => self.adv(operand),
-                1 => self.bxl(operand),
-                2 => self.bst(operand),
-                3 => self.jnz(operand),
-                4 => self.bxc(operand),
-                5 => self.out(operand),
-                6 => self.bdv(operand),
-                7 => self.cdv(operand),
-                _ => unreachable!(),
+            if let Some(output) = self.state.run_single_step(instruction, operand) {
+                self.output.push(output);
             }
         }
     }
+}
 
-    fn adv(&mut self, operand: u32) {
-        self.reg_a /= 2u32.pow(self.combo(operand));
+impl ComputerState {
+    pub fn run_single_step(&mut self, instruction: u64, operand: u64) -> Option<u8> {
+        match instruction {
+            0 => self.adv(operand),
+            1 => self.bxl(operand),
+            2 => self.bst(operand),
+            3 => self.jnz(operand),
+            4 => self.bxc(operand),
+            5 => {
+                let res = self.out(operand);
+                return Some(res);
+            }
+            6 => self.bdv(operand),
+            7 => self.cdv(operand),
+            _ => unreachable!(),
+        };
+
+        None
+    }
+    fn adv(&mut self, operand: u64) {
+        self.reg_a /= 2u64.pow(self.combo(operand) as u32);
         self.increment();
     }
 
-    fn bxl(&mut self, operand: u32) {
+    fn bxl(&mut self, operand: u64) {
         self.reg_b ^= operand;
         self.increment();
     }
 
-    fn bst(&mut self, operand: u32) {
+    fn bst(&mut self, operand: u64) {
         self.reg_b = self.combo(operand) % 8;
         self.increment();
     }
 
-    fn jnz(&mut self, operand: u32) {
+    fn jnz(&mut self, operand: u64) {
         if self.reg_a != 0 {
             self.pc = operand as usize;
         } else {
@@ -54,23 +70,23 @@ impl Computer {
         }
     }
 
-    fn bxc(&mut self, _operand: u32) {
+    fn bxc(&mut self, _operand: u64) {
         self.reg_b ^= self.reg_c;
         self.increment();
     }
 
-    fn out(&mut self, operand: u32) {
-        self.output.push((self.combo(operand) % 8) as u8);
+    fn out(&mut self, operand: u64) -> u8 {
+        self.increment();
+        (self.combo(operand) % 8) as u8
+    }
+
+    fn bdv(&mut self, operand: u64) {
+        self.reg_b = self.reg_a / 2u64.pow(self.combo(operand) as u32);
         self.increment();
     }
 
-    fn bdv(&mut self, operand: u32) {
-        self.reg_b = self.reg_a / 2u32.pow(self.combo(operand));
-        self.increment();
-    }
-
-    fn cdv(&mut self, operand: u32) {
-        self.reg_c = self.reg_a / 2u32.pow(self.combo(operand));
+    fn cdv(&mut self, operand: u64) {
+        self.reg_c = self.reg_a / 2u64.pow(self.combo(operand) as u32);
         self.increment();
     }
 
@@ -78,7 +94,7 @@ impl Computer {
         self.pc += 2;
     }
 
-    fn combo(&self, val: u32) -> u32 {
+    fn combo(&self, val: u64) -> u64 {
         match val {
             0..=3 => val,
             4 => self.reg_a,
@@ -89,7 +105,7 @@ impl Computer {
         }
     }
 }
-pub fn run<R>(reader: R) -> (String, usize)
+pub fn run<R>(reader: R) -> (String, u64)
 where
     R: BufRead,
 {
@@ -100,11 +116,17 @@ where
     lines.next();
     let instructions = parse_opcodes(&mut lines.next().unwrap().unwrap());
 
+    let a_value = 0u64;
+    let res = loop_de_loop(&instructions, a_value, instructions.len());
+    let res_p2 = res.unwrap();
+
     let mut computer = Computer {
-        pc: 0,
-        reg_a,
-        reg_b,
-        reg_c,
+        state: ComputerState {
+            pc: 0,
+            reg_a,
+            reg_b,
+            reg_c,
+        },
         instructions,
         output: Vec::new(),
     };
@@ -117,10 +139,42 @@ where
         .map(|val| ((val + b'0') as char).to_string())
         .join(",");
 
-    (res, 5)
+    (res, res_p2)
 }
 
-fn parse_reg(register_line: &str) -> u32 {
+fn loop_de_loop(instructions: &[u64], a_value: u64, curr_check_idx: usize) -> Option<u64> {
+    if curr_check_idx == 0 {
+        return Some(a_value);
+    }
+    let a_value = a_value << 3;
+    let instr = instructions[curr_check_idx - 1];
+    for test in 0..8 {
+        let test_reg_a = a_value + test;
+        let mut state = ComputerState {
+            pc: 0,
+            reg_a: test_reg_a,
+            reg_b: 0,
+            reg_c: 0,
+        };
+
+        let output = loop {
+            let instr = instructions[state.pc];
+            let op = instructions[state.pc + 1];
+            if let Some(output) = state.run_single_step(instr, op) {
+                break output;
+            }
+        };
+
+        if output as u64 == instr {
+            if let Some(a_val) = loop_de_loop(&instructions, test_reg_a, curr_check_idx - 1) {
+                return Some(a_val);
+            }
+        }
+    }
+    None
+}
+
+fn parse_reg(register_line: &str) -> u64 {
     register_line
         .split_whitespace()
         .last()
@@ -129,12 +183,12 @@ fn parse_reg(register_line: &str) -> u32 {
         .unwrap()
 }
 
-fn parse_opcodes(opcode_line: &str) -> Vec<u32> {
+fn parse_opcodes(opcode_line: &str) -> Vec<u64> {
     opcode_line
         .split_whitespace()
         .last()
         .unwrap()
         .split(",")
-        .map(|val| val.parse::<u32>().unwrap())
+        .map(|val| val.parse::<u64>().unwrap())
         .collect()
 }

12d58b51320a81c47e6ef6ba0689a315b17c7493
Kai Roßwag <kai.rosswag@horus.biz>
2024-12-18 21:59:28 +0100

    solve part 2

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -1,71 +1,117 @@
 use std::collections::BTreeSet;
-use hashbrown::{HashMap, HashSet};
 use std::io::BufRead;
 
-pub fn run<R>(reader: R) -> (usize, usize)
+const WIDTH: usize = 70usize;
+const HEIGHT: usize = 70usize;
+pub fn run<R>(reader: R) -> (usize, String)
 where
     R: BufRead,
 {
-    
-    let width = 71usize;
-    let height = 71usize;
-    let mut map = vec![usize::MAX - 1; width * height];
-
-    for line in reader.lines().take(1024) {
-        let line = line.unwrap();
-        let (x, y) = line.split_once(',').unwrap();
-        let x = x.parse::<usize>().unwrap();
-        let y = y.parse::<usize>().unwrap();
-
-
-        map[y * width + x] = usize::MAX;
+    let mut costing = [usize::MAX - 1; (WIDTH + 1) * (HEIGHT + 1)];
+    let mut walls = [false; (WIDTH + 1) * (HEIGHT + 1)];
+    let mut lines = reader.lines();
+    for _ in 0..1024 {
+        let line = lines.next().unwrap().unwrap();
+        let (x, y) = get_xy(&line);
+        walls[to_coord(x, y)] = true;
     }
 
-
-    let res_p1 = find_path(&mut map, width, height);
-
-    (res_p1, 5)
-
-}
-
-fn find_path(map: &mut [usize], width: usize, height: usize) -> usize {
-    map[0] = 0;
     let mut to_visit = BTreeSet::new();
     to_visit.insert((0, (0, 0)));
-    while let Some((cost, (x, y))) = to_visit.pop_first() {
-        if x == width - 1 && y == height - 1 {
-            return cost;
+    let res_p1 = find_path(&walls, &mut costing, &mut to_visit).unwrap();
+
+    let mut pos = 1024;
+    let res_p2 = loop {
+        let line = &lines.next().unwrap().unwrap();
+        let (x, y) = get_xy(&line);
+        walls[to_coord(x, y)] = true;
+        let threshold = costing[to_coord(x, y)];
+        if threshold == usize::MAX - 1 {
+            // threshold not in shortest path
+            continue;
         }
-
-        for (neighbor_x, neighbor_y) in neighbors(map, x, y, width, height) {
-            to_visit.insert((cost + 1, (neighbor_x, neighbor_y)));
+        costing[to_coord(x, y)] = usize::MAX - 1;
+        wipe_costing(&mut costing, threshold, &mut to_visit);
+        if let Some(val) = find_path(&walls, &mut costing, &mut to_visit) {
+            pos += 1;
+        } else {
+            break format!("{},{}", x, y);
         }
+    };
 
-    }
-
-    panic!("no path found");
+    (res_p1, res_p2)
 }
 
-fn neighbors(map: &[usize], x: usize, y: usize, width: usize, height: usize) -> Vec<(usize, usize)> {
+fn wipe_costing(costing: &mut [usize], threshold: usize, to_visit: &mut BTreeSet<(usize, (usize, usize))>) {
+    while let Some(option) = to_visit.last() {
+        if option.0 > threshold {
+            to_visit.pop_last();
+        } else {
+            break;
+        }
+    }
+    for y in 0..=HEIGHT {
+        for x in 0..=WIDTH {
+            let val = &mut costing[to_coord(x, y)];
+            if *val > threshold {
+                *val = usize::MAX - 1;
+            } else if *val == threshold {
+                *val = usize::MAX - 1;
+                to_visit.insert((threshold, (x, y)));
+            }
+        }
+    }
+}
+
+fn get_xy(line: &str) -> (usize, usize) {
+    let (x, y) = line.split_once(',').unwrap();
+    let x = x.parse::<usize>().unwrap();
+    let y = y.parse::<usize>().unwrap();
+
+    (x, y)
+}
+
+fn find_path(
+    walls: &[bool],
+    costing: &mut [usize],
+    to_visit: &mut BTreeSet<(usize, (usize, usize))>,
+) -> Option<usize> {
+    while let Some((cost, (x, y))) = to_visit.pop_first() {
+        costing[to_coord(x, y)] = cost;
+        if x == WIDTH && y == HEIGHT {
+            return Some(cost);
+        }
+
+        for (neighbor_x, neighbor_y) in neighbors(walls, costing, x, y) {
+            to_visit.insert((cost + 1, (neighbor_x, neighbor_y)));
+        }
+    }
+
+    None
+}
+
+fn neighbors(walls: &[bool], costing: &[usize], x: usize, y: usize) -> Vec<(usize, usize)> {
     let mut neighbors = Vec::new();
-    let can_visit = |x, y| map[y * width + x] == usize::MAX - 1;
+    let can_visit = |x, y| {
+        let pos = to_coord(x, y);
+        !walls[pos] && costing[pos] == usize::MAX - 1
+    };
     if x > 0 && can_visit(x - 1, y) {
         neighbors.push((x - 1, y));
     }
-    if x < width - 1 && can_visit(x + 1, y) {
+    if x < WIDTH && can_visit(x + 1, y) {
         neighbors.push((x + 1, y));
     }
     if y > 0 && can_visit(x, y - 1) {
         neighbors.push((x, y - 1));
     }
-    if y < height - 1 && can_visit(x, y + 1) {
+    if y < HEIGHT && can_visit(x, y + 1) {
         neighbors.push((x, y + 1));
     }
-
 
     neighbors
 }
 
-
-
-
+fn to_coord(x: usize, y: usize) -> usize {
+    y * (WIDTH + 1) + x
+}
